c94fdf42664d3d198a19f1ab61679410e9af1e73
ajit283 <55892788+ajit283@users.noreply.github.com>
2024-03-09 21:46:17 +0100

    fix quo 2

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -25,10 +25,10 @@ export default function Quote({
           </div>
         </span>
       </h2>
-      <div className="flex flex-row justify-between gap-5">
+      <div className="flex flex-col justify-between gap-5">
         <p className=" italic"> - {author}</p>
         <button
-          className="  flex-grow-0 bg-gradient-to-br hover:bg-gray-200   border-2 border-gray-200 hover:text-black  py-2 px-4 rounded-lg"
+          className=" self-start  flex-grow-0 bg-gradient-to-br hover:bg-gray-200   border-2 border-gray-200 hover:text-black  py-2 px-4 rounded-lg"
           onClick={() => {
             copyToClipboard(`${quote} - ${author}`);
             setCopied(true);
